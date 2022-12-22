0eaea52d124973cbd27874c3cb741e99a7d2b01c
Ricardo Sanz <ricardosanz@quibim.com>
2022-12-22 08:45:40 +0100

    Initialize the Angular workspace with an Angular component

---
+++ scope/ui/my-button/my-button.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-button',
+  templateUrl: './my-button.component.html',
+  styleUrls: ['./my-button.component.scss']
+})
+export class MyButtonComponent {
+  constructor() {}
+
+  logMessage(): void {
+    console.log('have a good day!')
+  }
+}
