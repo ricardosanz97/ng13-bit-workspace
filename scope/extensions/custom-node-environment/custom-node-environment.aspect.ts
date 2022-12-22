0ec349db8edd2bb64a43549dd7eae9fe5539dfc6
Ricardo Sanz <ricardosanz@quibim.com>
2022-12-22 16:43:44 +0100

    Remove the express app component and its environment

--- scope/extensions/custom-node-environment/custom-node-environment.aspect.ts
+++
@@ -1,5 +0,0 @@
-import { Aspect } from '@teambit/harmony';
-
-export const CustomNodeEnvironmentAspect = Aspect.create({
-  id: 'company.scope/extensions/custom-node-environment',
-});
--- scope/extensions/custom-node-environment/index.ts
+++
@@ -1,5 +0,0 @@
-import { CustomNodeEnvironmentAspect } from './custom-node-environment.aspect';
-
-export type { CustomNodeEnvironmentMain } from './custom-node-environment.main.runtime';
-export default CustomNodeEnvironmentAspect;
-export { CustomNodeEnvironmentAspect };
